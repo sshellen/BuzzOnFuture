2aa443d16eb3df8381c41000e1515ee78cd68bf6
Steinar <sshellen@users.noreply.github.com>
2023-01-23 16:18:00 -0800

    Deleting components dir

--- src/Components/ImageLoader.js
+++
@@ -1,8 +0,0 @@
-import React, { Suspense, lazy } from "react";
-import Loader from "./Loader";
-
-const ImageLoader = ({ name }) => {
-  return <img src={`/svg/${name}.svg`} />;
-};
-
-export default ImageLoader;
--- src/Components/Slug.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Slug = () => {
-  return <div className="slug" />;
-};
-
-export default Slug;
